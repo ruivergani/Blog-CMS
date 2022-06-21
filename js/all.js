2b04b4febebdbeb775b258fd82a83575930c7976
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-06-21 11:43:50 -0300

    swiper slide completed html

--- js/all.js
+++ js/all.js
@@ -1,1 +1,1 @@
-"use strict";var header=document.getElementById("header");$(function(){$(window).on("scroll",function(){20<$(window).scrollTop()?$("#header").addClass("bgON"):$("#header").removeClass("bgON")})});
+"use strict";var header=document.getElementById("header");$(function(){$(window).on("scroll",function(){20<$(window).scrollTop()?$("#header").addClass("bgON"):$("#header").removeClass("bgON")})});var slide_podcast=new Swiper(".slide-podcast",{slidesPerView:4,spaceBetween:32,pagination:{el:".s-podcast .container .top .ctrl-slide .swiper-pagination",clickable:!0}});
--- js/scripts/main.js
+++ js/scripts/main.js
@@ -10,3 +10,13 @@ $(function() {
       }
   });
 });
+
+// Swiper Pagination Slide
+var slide_podcast = new Swiper(".slide-podcast", {
+  slidesPerView: 4,
+  spaceBetween: 32,
+  pagination: {
+    el: ".s-podcast .container .top .ctrl-slide .swiper-pagination",
+    clickable: true,
+  },
+});
